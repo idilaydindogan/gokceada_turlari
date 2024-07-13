a68b619b8f5c68dfb05975ecaedec8b291eb8fec
idilaydin <idilaydindogan@gmail.com>
2024-07-13 17:00:00 +0300

    Homepage added

--- gokceada_tours/src/app/page.js
+++ gokceada_tours/src/app/page.js
@@ -1,7 +1,10 @@
+import HomePage from "@/components/HomePage";
 import Image from "next/image";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+		<main className="flex flex-col items-center justify-between">
+			<HomePage />
+		</main>
 	);
 }
